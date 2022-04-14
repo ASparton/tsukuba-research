83b4850eafe8a7aa7fed558ec8a514bbdababfbd
Alexandre Sparton <sparton.alexandre@gmail.com>
2022-04-14 16:30:50 +0200

    TMSI done + first noisy & rotation inputs done

--- src/inputs/preprocessing.py
+++ src/inputs/preprocessing.py
@@ -1,4 +1,6 @@
 import numpy
+from skimage.util import random_noise
+from skimage.transform import rotate
 
 def normalize_pixels(pixel_matrix : numpy.ndarray) -> numpy.ndarray:
     """Divide all the values inside the given ndarray by 255 to normalize pixels value."""
@@ -14,3 +16,9 @@ def get_cnn_prepared_features(features : numpy.ndarray) -> numpy.ndarray:
     # We add just one channel as we stay with greyscale images
     
     return normalize_pixels(features).reshape(-1, 28, 28, 1)
+
+def get_rotated_images(images : numpy.ndarray) -> numpy.ndarray :
+    return numpy.array([rotate(image, angle=90) for image in images])
+
+def get_noisy_images(images : numpy.ndarray) -> numpy.ndarray :
+    return numpy.array([random_noise(image, mode='salt', amount=0.001) for image in images])
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # External libraries imports
 import numpy
 from tensorflow.keras.datasets.mnist import load_data as load_mnist
+from statistics import mode
 
 # Internal imports
 import inputs.preprocessing as ip
@@ -17,10 +18,32 @@ def get_nb_classification_error(model_predictions: numpy.ndarray, expected_outpu
             nb_errors += 1
     return nb_errors
 
+def get_system_predictions(cnn_predictions : list[int], knn_predictions : list[int], rf_predictions : list[int]) -> list[int] :
+    """Gets the system predictions by voting system"""
+    
+    system_predictions = []
+    for i in range(len(cnn_predictions)):
+        current_predictions = [cnn_predictions[i], knn_predictions[i], rf_predictions[i]]
+        system_predictions.append(mode(current_predictions))
+    return system_predictions
+
+def get_system_accuracy(nb_errors : int, nb_predictions : int) -> float :
+    """Computes the system accuracy"""
+    
+    return 1 - (nb_errors / nb_predictions)
+
 def main():
     # DATA
     # Get dataset + split train and test data
     (x_train, y_train), (x_test, y_test) = load_mnist()
+    
+    
+    # UNCOMMENT TO USE NOISY OR ROTATED IMAGES
+    # x_train = ip.get_noisy_images(x_train)
+    # x_test = ip.get_noisy_images(x_test)
+    
+    # x_train = ip.get_rotated_images(x_train)
+    # x_test = ip.get_rotated_images(x_test)
     
     # Prepare data for the CNN model (normalization & reshaping)
     x_cnn_train = ip.get_cnn_prepared_features(x_train)
@@ -31,6 +54,7 @@ def main():
     x_sklearn_test = x_test.reshape(-1, 28 * 28)
     
     # MODELS
+    print("\n-----Models building and training-----")
     # CNN
     cnn_model = cnn.build_model()
     cnn.train_model(cnn_model, x_cnn_train, y_train, x_cnn_test, y_test)
@@ -48,6 +72,7 @@ def main():
     print(f"Random forest --> Model accuracy: {rf_accuracy}")
     
     # PREDICTIONS
+    print("\n-----Models predictions-----")
     # CNN
     cnn_predictions = cnn.get_predictions(cnn_model, x_cnn_test)
     print(f"\nCNN --> Number of wrong classification: {get_nb_classification_error(cnn_predictions, y_test)} / 10000.")
@@ -59,6 +84,15 @@ def main():
     # Random forest
     rf_predictions = rf_model.predict(x_sklearn_test)
     print(f"Random forest --> Number of wrong classification: {get_nb_classification_error(rf_predictions, y_test)} / 10000.")
+    
+    # SYSTEMS RESULTS
+    print("\n-----System results-----")
+    # TMSI
+    tmsi_predictions = get_system_predictions(cnn_predictions, knn_predictions, rf_predictions)
+    nb_tmsi_errors = get_nb_classification_error(tmsi_predictions, y_test)
+    tmsi_accuracy = get_system_accuracy(nb_tmsi_errors, len(y_test))
+    print(f"\nTMSI --> System accuracy: {tmsi_accuracy}")
+    print(f"TMSI --> Number of wrong classification: {nb_tmsi_errors} / 10000.")
 
 if __name__ == '__main__':
     main()
